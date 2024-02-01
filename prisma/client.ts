b3a9634f9f3a79fd3e0716a2417082a070b4e1ed
leozhou <492753662@qq.com>
2024-02-01 12:46:05 +0800

    build an API for creating

---
+++ prisma/client.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client'
+
+
+/// 创建一个单例的 PrismaClient 实例
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
